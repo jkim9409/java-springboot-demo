33f650f5abadea04031b4626db0e8102cfb370a3
jkim9409 <dev.junkim@gmail.com>
2023-02-14 10:51:01 +0900

    first commit

---
+++ src/test/java/myfirst/javaspringbootdemo/JavaSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirst.javaspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
